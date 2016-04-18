7bbebcc3e1de56ab57d40cd66b22848a560110f5
spencer.gennari <sdgennari@gmail.com>
2016-04-17 21:47:33 -0400

    Added settings UI for regions and locales

--- lol-champ-selector/app/src/main/java/com/poofstudios/android/lolchampselector/LocationActivity.java
+++ lol-champ-selector/app/src/main/java/com/poofstudios/android/lolchampselector/LocationActivity.java
@@ -3,6 +3,7 @@ package com.poofstudios.android.lolchampselector;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -14,9 +15,16 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -25,6 +33,18 @@ public class LocationActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_LOCATION = 0;
 
     private LocationManager mLocationManager;
+    private Spinner mRegionSpinner;
+    private Spinner mLocaleSpinner;
+    private Switch mLocationSwitch;
+
+    private Resources mRes;
+
+    private List<String> mRegionList;
+    private List<String> mRegionDataList;
+    private List<String> mLocaleList;
+    private List<String> mLocaleDataList;
+    private ArrayAdapter<String> mLocaleAdapter;
+    private String currentLocaleData = null;
 
     private final LocationListener mLocationListener = new LocationListener() {
         @Override
@@ -33,9 +53,6 @@ public class LocationActivity extends AppCompatActivity {
 
             // Handle the location accordingly
             handleLocation(location);
-
-            // Stop requesting location updates after receiving the first location
-            stopLocationUpdates();
         }
 
         @Override
@@ -56,25 +73,26 @@ public class LocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
+        // Create new location manager
         mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        Log.d("LOL", "onCreate");
 
-        // Check that the permissions have been granted
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Log.d("LOL", "Starting location manager");
+        // Load arrays from resources
+        mRes = getResources();
+        mRegionList = Arrays.asList(mRes.getStringArray(R.array.regions));
+        mRegionDataList = Arrays.asList(mRes.getStringArray(R.array.regions_data));
+        mLocaleList = Arrays.asList(mRes.getStringArray(R.array.locales_na));
+        mLocaleDataList = Arrays.asList(mRes.getStringArray(R.array.locales_na_data));
 
-            // Register the LocationManager to receive updates
-            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+        // Get views from the layout
+        mLocationSwitch = (Switch) findViewById(R.id.location_switch);
+        mRegionSpinner = (Spinner) findViewById(R.id.spinner_region);
+        mLocaleSpinner = (Spinner) findViewById(R.id.spinner_locale);
 
-            // Use GPS provider when testing on the emulator
-            // mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
-        } else {
-            // If not, request the permission explicitly
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSION_REQUEST_LOCATION);
-        }
+        // Configure data and listeners for the views
+        configureViews();
+
+        // Create the location manager
+//        startLocationManager();
     }
 
     @Override
@@ -94,10 +112,10 @@ public class LocationActivity extends AppCompatActivity {
                     // Note: Recheck permissions here to prevent Android Studio error from displaying
                     if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
                             ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+//                        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
 
                         // Use GPS provider when testing on the emulator
-                        // mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+                         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
                     }
 
                 } else {
@@ -107,6 +125,128 @@ public class LocationActivity extends AppCompatActivity {
             break;
             default:
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void configureViews() {
+        mLocationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // User has selected to get region and locale from GPS
+                    mRegionSpinner.setEnabled(false);
+                    mLocaleSpinner.setEnabled(false);
+                    startLocationManager();
+
+                } else {
+                    // User has selected to manually input region and locale
+                    mRegionSpinner.setEnabled(true);
+                    mLocaleSpinner.setEnabled(true);
+                    stopLocationUpdates();
+                }
+            }
+        });
+
+        // Region spinner
+        ArrayAdapter<String> regionAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, mRegionList);
+        regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mRegionSpinner.setAdapter(regionAdapter);
+        mRegionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Get the new locale list and locale data list based on the item selected
+                switch (position) {
+                    case 0:
+                        mLocaleList = Arrays.asList(mRes.getStringArray(R.array.locales_na));
+                        mLocaleDataList = Arrays.asList(mRes.getStringArray(R.array.locales_na_data));
+                        break;
+                    case 1:
+                        mLocaleList = Arrays.asList(mRes.getStringArray(R.array.locales_euw));
+                        mLocaleDataList = Arrays.asList(mRes.getStringArray(R.array.locales_euw_data));
+                        break;
+                    case 2:
+                        mLocaleList = Arrays.asList(mRes.getStringArray(R.array.locales_jp));
+                        mLocaleDataList = Arrays.asList(mRes.getStringArray(R.array.locales_jp_data));
+                        break;
+                    case 3:
+                        mLocaleList = Arrays.asList(mRes.getStringArray(R.array.locales_kr));
+                        mLocaleDataList = Arrays.asList(mRes.getStringArray(R.array.locales_kr_data));
+                        break;
+                    default:
+                        mLocaleList = Arrays.asList(mRes.getStringArray(R.array.locales_na));
+                        mLocaleDataList = Arrays.asList(mRes.getStringArray(R.array.locales_na_data));
+                        break;
+                }
+
+                // Update locale spinner
+                mLocaleAdapter = new ArrayAdapter<>(LocationActivity.this,
+                        android.R.layout.simple_spinner_item, mLocaleList);
+                mLocaleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                mLocaleSpinner.setAdapter(mLocaleAdapter);
+
+                // Update locale spinner selection if applicable
+                int localeIdx = mLocaleDataList.indexOf(currentLocaleData);
+                if (localeIdx != -1) {
+                    mLocaleSpinner.setSelection(localeIdx);
+                } else {
+                    mLocaleSpinner.setSelection(0);
+                }
+
+                // Disable the spinner if only 1 locale is available
+                if (mLocaleList.size() == 1) {
+                    mLocaleSpinner.setEnabled(false);
+                } else {
+                    mLocaleSpinner.setEnabled(true);
+                }
+
+                // TODO Update preferences
+                Log.d("LOL", "Update region preferences");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+        // Locale spinner
+        mLocaleAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, mLocaleList);
+        mLocaleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mLocaleSpinner.setAdapter(mLocaleAdapter);
+        mLocaleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentLocaleData = mLocaleDataList.get(position);
+
+                // TODO Update preferences
+                Log.d("LOL", "Update locale preferences: " + currentLocaleData);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+    }
+
+    private void startLocationManager() {
+        // Check that the permissions have been granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("LOL", "Starting location manager");
+
+            // Register the LocationManager to receive updates
+//            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+
+            // Use GPS provider when testing on the emulator
+             mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+        } else {
+            // If not, request the permission explicitly
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
         }
     }
 
@@ -122,14 +262,20 @@ public class LocationActivity extends AppCompatActivity {
         }
 
         if (addresses == null || addresses.size() == 0) {
-            // TODO Handle no addresses found
+            // No addresses found
         } else {
             Address address = addresses.get(0);
             String countryCode = address.getCountryCode();
             String apiRegion = getAPIRegionFromCountryCode(countryCode);
-            String apiLocale = getAPILocaleFroMCountryCode(countryCode);
-            Log.d("LOL", "Region: " + apiRegion);
-            Log.d("LOL", "Locale: " + apiLocale);
+            currentLocaleData = getAPILocaleFromCountryCode(countryCode);
+
+            // Update the region spinner
+            // Note: This will update the new set of locales via the selection listener
+            int regionIdx = mRegionDataList.indexOf(apiRegion);
+            mRegionSpinner.setSelection(regionIdx);
+
+            // Stop requesting location updates after the region and locale have been found
+            stopLocationUpdates();
         }
     }
 
@@ -152,7 +298,7 @@ public class LocationActivity extends AppCompatActivity {
         }
     }
 
-    private String getAPILocaleFroMCountryCode(String countryCode) {
+    private String getAPILocaleFromCountryCode(String countryCode) {
         switch (countryCode) {
             case "US":              // United States
                 return "en_US";
@@ -178,6 +324,7 @@ public class LocationActivity extends AppCompatActivity {
     private void stopLocationUpdates() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("LOL", "Stopping location updates");
             mLocationManager.removeUpdates(mLocationListener);
         }
     }
